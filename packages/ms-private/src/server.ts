60f8ee724f60037cef6cfc4791c31f1e11af5f34
Lucas Yuri <lucasyuri56@hotmail.com>
2020-09-22 11:53:23 -0300

    Add erros and hasPermission middlewares

--- packages/ms-private/src/server.ts
+++ packages/ms-private/src/server.ts
@@ -1,19 +1,27 @@
 import express from 'express';
+import hasPermissions from './middlewares/hasPermission';
+import Handler from './exceptions/Handler';
 
 const app = express();
 
 const routes = express.Router();
 
-
-routes.get('/private', (request, response) => {
+routes.get(
+  '/private',
+  hasPermissions(['manage-vouchers']),
+  (request, response) => {
     return response.json({
-        mensage: 'ok',
-        ms: 'private'
-    })
-})
+      mensage: 'ok',
+      ms: 'private',
+    });
+  },
+);
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3334, () => console.log('Private microservice running'));
+const handler = new Handler();
 
+app.use(handler.handleError);
+
+app.listen(3334, () => console.log('Private microservice running'));
